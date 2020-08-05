6d97facb9b2396dab27e64703019ab6510ac15dd
MrPoketes <az778@york.ac.uk>
2020-08-05 21:05:51 +0300

    added procfile

--- src/server.js
+++ src/server.js
@@ -42,17 +42,4 @@ app.use("/auth", authenticationRoutes);
 
 // Server launch
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
-
 app.listen(process.env.PORT, () => console.log("Listening on port process.env.PORT"));

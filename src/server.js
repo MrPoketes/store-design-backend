2f5f9dfec378f3c2754456099143378b07cc8fe8
MrPoketes <az778@york.ac.uk>
2020-08-13 22:56:43 +0300

    some minor changes to fix for heroku to run

--- src/server.js
+++ src/server.js
@@ -9,24 +9,26 @@ const productRoutes = require('./api/routes/products');
 const passport = require("passport");
 const authenticationRoutes = require("./api/routes/authentication");
 const basketRoutes = require("./api/routes/basket");
+const PORT = process.env.PORT || 8081;
 
 // Connecting to mongoose.
 // TODO : when deploying to a website change the url
 
-mongoose.connect(process.env.MONGODB_URI, { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
+const URI = 'mongodb://localhost:37017/shop';
+mongoose.connect(process.env.MONGODB_URI || URI, { useFindAndModify: false, useUnifiedTopology: true, useNewUrlParser: true }).then(() => {
     console.log("Successfully connected to MongoDB");
 });
 
 mongoose.Promise = global.Promise;
 
 // App use
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// TODO : Change origin when deploying to heroku
 app.use(cors({
-    origin: "https://store-design.herokuapp.com",
+    origin: "http://store-design.herokuapp.com, http://localhost:3000",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
     credentials: true,
 }));
 
@@ -42,4 +44,4 @@ app.use("/auth", authenticationRoutes);
 
 // Server launch
 
-app.listen(process.env.PORT, () => console.log("Listening on port process.env.PORT"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

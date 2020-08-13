e029df6f306f596b9004d98cec397d1b7a21bcac
MrPoketes <az778@york.ac.uk>
2020-08-14 00:06:57 +0300

    trying to fix cors

--- src/server.js
+++ src/server.js
@@ -25,12 +25,26 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors({
-    origin: "http://store-design.herokuapp.com",
-    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
-    credentials: true,
-}));
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+// app.use(cors({
+//     origin: "http://store-design.herokuapp.com",
+//     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+//     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+//     credentials: true,
+// }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
